7de66b7a5c341bbe2bdf720f50be593fc9e96a04
Paul <pgwar2@gmail.com>
2024-06-02 15:35:47 -0700

    add readme, twitter confgis update ignore

---
+++ configs/twitter_details.py
@@ -0,0 +1,53 @@
+twitter_auth = {
+    'jim': {
+        'handle': '@JmHalp',
+        'api_key': '',
+        'api_key_secret':'',
+        'bearer_token': '',
+        'access_token': '',
+        'access_token_secret': '',
+        'oauth_client_id':'',
+        'oauth_token_secret':''
+    },
+    'michael': {
+        'handle': '@BestBossNWorld',
+        'api_key': '',
+        'api_key_secret':'',
+        'bearer_token': '',
+        'access_token': '',
+        'access_token_secret': '',
+        'oauth_client_id':'',
+        'oauth_token_secret':''
+    },
+    'pam': {
+        'handle': '@TheArtistPam',
+        'api_key': '',
+        'api_key_secret':'',
+        'bearer_token': '',
+        'access_token': '',
+        'access_token_secret': '',
+        'oauth_client_id':'',
+        'oauth_token_secret':''
+    },
+    'stanley': {
+        'handle': '@hudson_sta4680',
+        'api_key': '',
+        'api_key_secret':'',
+        'bearer_token': '',
+        'access_token': '',
+        'access_token_secret': '',
+        'oauth_client_id':'',
+        'oauth_token_secret':''
+    },
+    'dwight': {
+        'handle': '@BeetBattler',
+        'api_key': '',
+        'api_key_secret':'',
+        'bearer_token': '',
+        'access_token': '',
+        'access_token_secret': '',
+        'oauth_client_id':'',
+        'oauth_token_secret':''
+    }
+}
+
